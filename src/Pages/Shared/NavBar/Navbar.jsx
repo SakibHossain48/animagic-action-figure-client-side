5f4888b122f1149c5abd88f940b15835a934a4db
Sakib Hossain <115476707+SakibHossain48@users.noreply.github.com>
2023-06-10 22:08:30 +0600

    Update Navbar.jsx

--- src/Pages/Shared/NavBar/Navbar.jsx
+++ src/Pages/Shared/NavBar/Navbar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
             <div className="">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 " stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <div tabIndex={0} className='md:space-x-8 menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-28'>
                         <Link className={location.pathname === '/' && 'text-white'} to='/'>Home</Link>
